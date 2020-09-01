a81a488fd1382c25b5c2b8f3c83571e6d91e90cc
jackle91 <hieu6@gatech.edu>
2020-09-01 01:19:03 -0400

    add pinpad signIn

---
+++ app/src/main/java/com/example/jackle91/myposservice/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.jackle91.myposservice;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.hanks.passcodeview.PasscodeView;
+
+public class MainActivity extends AppCompatActivity {
+
+    PasscodeView passcodeView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        passcodeView = findViewById(R.id.passcodeView);
+        passcodeView.setPasscodeLength(4)
+                .setLocalPasscode("1234")
+                .setListener(new PasscodeView.PasscodeViewListener() {
+                    @Override
+                    public void onFail() {
+                        Toast.makeText(getApplicationContext(), "Password is Wrong", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onSuccess(String number) {
+                        Intent intent = new Intent(MainActivity.this, WelcomeActivity.class);
+                        startActivity(intent);
+                    }
+                });
+    }
+}
